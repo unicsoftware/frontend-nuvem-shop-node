7c96ec25a2471033cbbfcc117d59119f7fcf42d6
unicsoftware <elpidio.junior@gmail.com>
2023-10-31 20:38:22 -0300

    alterar Porta Site

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    port: 3000
+  },
   optimizeDeps: {
     include: [
       '@nimbus-ds/patterns',
